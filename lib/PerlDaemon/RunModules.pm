e49102eea89f9f7b44945401882e7284b593f7cf
Paul Buetow <paul@buetow.org>
2011-05-13 14:30:51 +0000

    added initial README file plus some other stuff

--- lib/PerlDaemon/RunModules.pm
+++ lib/PerlDaemon/RunModules.pm
@@ -10,6 +10,9 @@ sub new ($$$) {
 
         if (-d $modulesdir) {
                 $logger->logmsg("Loading modules from $modulesdir");        
+
+        } else {
+                $logger->warn("Modules dir $modulesdir does not exist!");        
         }
 
         return $self;
@@ -19,6 +22,8 @@ sub do ($) {
 	my $self = shift;
 	my $conf = $self->{conf};
 	my $logger = $conf->{logger};
+
+        $logger->warn("No modules are loaded!");
 }
 
 1;
