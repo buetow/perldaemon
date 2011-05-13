816031ccd4a0bf5e4a5e4fa8c28b9b68a3b71d40
Paul Buetow <paul@buetow.org>
2011-05-13 14:06:19 +0000

    ./bin contains the start script
    control.sh is just a wrapper

--- control.sh
+++ control.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-case $1 in
-	start)
-	perl PerlDaemon/PerlDaemon.pl ./conf/perldaemon.conf
-	;;
-
-	stop)
-	kill $(cat ./run/perldaemon.pid)
-	;;
-
-	logrotate)
-	kill -HUP $(cat ./run/perldaemon.pid)
-	;;
-
-	*)
-	echo "Usage: $0 <start|stop|logrotate>"
-	exit 1
-	;;
-esac
+exec ./bin/perldaemon $@
