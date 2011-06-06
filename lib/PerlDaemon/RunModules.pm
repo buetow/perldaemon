2bcd3cd68643c258aeeec47dbbdfeff57e3a218e
Paul Buetow <paul@buetow.org>
2011-06-06 07:04:43 +0000

    Initial correct handling of time carryings

--- lib/PerlDaemon/RunModules.pm
+++ lib/PerlDaemon/RunModules.pm
@@ -2,6 +2,8 @@
 
 package PerlDaemon::RunModules;
 
+use strict;
+use warnings;
 use Time::HiRes qw(gettimeofday tv_interval);
 
 sub new ($$) {
@@ -33,6 +35,8 @@ sub new ($$) {
                         } else {
                                 $loadedmodules{$name} = $module;
                                 $scheduler{$name}{lastrun} = [0,0];
+                                $scheduler{$name}{carrysum} = 0;
+                                $scheduler{$name}{firstloop} = 1;
                                 $scheduler{$name}{interval} = $conf->{'daemon.modules.runinterval'};
                                 $logger->logmsg("Created module instance $name");
                         }
@@ -63,14 +67,17 @@ sub do ($) {
                         my $now = [gettimeofday];
                         my $timediff = tv_interval($scheduler->{$k}{lastrun}, $now);
                         my $interval = $scheduler->{$k}{interval};
+                        my $carrytimediff = $timediff + $scheduler->{$k}{carrysum};
 
-                        if ($timediff >= $interval) {
-                                my $carry = $timediff - $interval;
+                        if ($carrytimediff >= $interval) {
+                                my $carry = $carrytimediff - $interval;
                                 $logger->logmsg("Triggering $k (last triggered before ${timediff}s; carry: ${carry}s; wanted interval: ${interval}s)");
 
-                                # TODO: carry handling 
-                                #if ($firstloop == 0) {
-                                #}
+                                if ($scheduler->{$k}{firstloop} == 0) {
+                                        $scheduler->{$k}{carrysum} = $carry;
+                                } else {
+                                        $scheduler->{$k}{firstloop} = 0;
+                                }
 
                                 $scheduler->{$k}{lastrun} = $now;
                                 $v->do();
@@ -78,7 +85,6 @@ sub do ($) {
                         }
                 }
 
-                $firstloop = 0;
         }
 }
 
